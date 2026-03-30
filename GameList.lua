07717e1ca16e0aea2a05c7d2a5836441f5ccd750
VoidDeveloper67 <juniortype123@gmail.com>
2026-03-30 20:19:57 +0100

    Update GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -1,6 +1,6 @@
 local Games = {
   [8202280624] = "https://raw.githubusercontent.com/VoidDeveloper67/VoidHub-Scripts/refs/heads/main/Bite%20By%20Night", -- Bite By Night
-  [119987266683883] = "https://raw.githubusercontent.com/VoidDeveloper67/VoidHub-Scripts/refs/heads/main/survive%20lava%20for%20brainrots.lua", -- Survive Lava for Brainrots
+  [9649298941] = "https://raw.githubusercontent.com/VoidDeveloper67/VoidHub-Scripts/refs/heads/main/survive%20lava%20for%20brainrots.lua", -- Survive Lava for Brainrots
   [109983668079237] = "https://raw.githubusercontent.com/VoidDeveloper67/VoidHub-Scripts/refs/heads/main/SAB%20Duels", -- SAB Duels
 }
 
