c538feb6d022418700b0da76815d232ca52cd69d
VoidDeveloper67 <juniortype123@gmail.com>
2026-04-02 11:40:31 +0100

    Update GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -1,5 +1,5 @@
 local Games = {
-  [8202280624] = "https://api.luarmor.net/files/v4/loaders/de2ac30145e59fa2750bf006440f3a42.lua", -- Bite By Night
+  [8202280624] = "https://raw.githubusercontent.com/VoidDeveloper67/VoidHub-Scripts/refs/heads/main/Bite%20By%20Night", -- Bite By Night
   [9649298941] = "https://raw.githubusercontent.com/VoidDeveloper67/VoidHub-Scripts/refs/heads/main/survive%20lava%20for%20brainrots.lua", -- Survive Lava for Brainrots
   [7709344486] = "https://raw.githubusercontent.com/VoidDeveloper67/VoidHub-Scripts/refs/heads/main/SAB%20Duels", -- SAB Duels
   [9186719164] = "https://api.luarmor.net/files/v4/loaders/beedef47976b519a1fd6ad005573710c.lua", -- Sailor Piece
