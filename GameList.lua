1fb8995f2dca707a34e47881f21a2d9e9be93075
VoidDeveloper67 <juniortype123@gmail.com>
2026-04-03 12:10:38 +0100

    Update GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -3,6 +3,7 @@ local Games = {
   [9649298941] = "https://raw.githubusercontent.com/VoidDeveloper67/VoidHub-Scripts/refs/heads/main/survive%20lava%20for%20brainrots.lua", -- Survive Lava for Brainrots
   [7709344486] = "https://raw.githubusercontent.com/VoidDeveloper67/VoidHub-Scripts/refs/heads/main/SAB%20Duels", -- SAB Duels
   [9186719164] = "https://api.luarmor.net/files/v4/loaders/beedef47976b519a1fd6ad005573710c.lua", -- Sailor Piece
+  [994732206]  = "https://raw.githubusercontent.com/VoidDeveloper67/VoidHub-Scripts/refs/heads/main/VoidHub%20PvP", -- Blox Fruits
 }
 
 return Games
