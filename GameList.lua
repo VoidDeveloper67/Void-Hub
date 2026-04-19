21f348b71e9bfd0b51daed481c1e5137e9ddecdb
VoidDeveloper67 <juniortype123@gmail.com>
2026-04-20 00:22:59 +0100

    Update GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -2,7 +2,7 @@ local Games = {
   [8202280624] = "https://raw.githubusercontent.com/VoidDeveloper67/VoidHub-Scripts/refs/heads/main/Bite%20By%20Night", -- Bite By Night
   [9649298941] = "https://raw.githubusercontent.com/VoidDeveloper67/VoidHub-Scripts/refs/heads/main/survive%20lava%20for%20brainrots.lua", -- Survive Lava for Brainrots
   [7709344486] = "https://raw.githubusercontent.com/VoidDeveloper67/VoidHub-Scripts/refs/heads/main/SAB%20Duels", -- SAB Duels
-  [9186719164] = "https://api.luarmor.net/files/v4/loaders/beedef47976b519a1fd6ad005573710c.lua", -- Sailor Piece
+  [9186719164] = "https://raw.githubusercontent.com/VoidDeveloper67/VoidHub-Scripts/refs/heads/main/Sailor%20Piece", -- Sailor Piece
   [994732206]  = "https://raw.githubusercontent.com/VoidDeveloper67/VoidHub-Scripts/refs/heads/main/VoidHub%20PvP", -- Blox Fruits
   [9787206684] = "https://raw.githubusercontent.com/VoidDeveloper67/VoidHub-Scripts/refs/heads/main/Be%20A%20Lucky%20Block", -- Be a Lucky Block
 }
