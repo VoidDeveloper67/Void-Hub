98c7597c31e7ca361dd47fc2fe69bdff71634083
VoidDeveloper67 <juniortype123@gmail.com>
2026-05-09 16:36:38 +0100

    Add Survive Zombie Arena script to GameList

--- GameList.lua
+++ GameList.lua
@@ -8,6 +8,7 @@ local Games = {
   [9098570654] = "https://raw.githubusercontent.com/VoidDeveloper67/VoidHub-Scripts/refs/heads/main/STA.luau", -- Survive The Apocalypse
   [10004244222] = "https://api.luarmor.net/files/v4/loaders/3804f87ddf39261103b63af76f47e943.lua", -- Kick a Lucky Block
   [9792947201] = "https://raw.githubusercontent.com/VoidDeveloper67/VoidHub-Scripts/refs/heads/main/Rng-Slime.luau", -- Slime RNG
+  [9348272796] = "https://raw.githubusercontent.com/VoidDeveloper67/VoidHub-Scripts/refs/heads/main/Survive%20Zombie%20Arena.luau", -- Survive Zombie Arena
 }
 
 return Games
