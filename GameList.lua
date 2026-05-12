f12cb266fc6a4ffa0dd98c2e716d65cf80f4bdd9
VoidDeveloper67 <juniortype123@gmail.com>
2026-05-12 19:34:57 +0100

    Update URL for 'Kick a Lucky Block' script

--- GameList.lua
+++ GameList.lua
@@ -6,7 +6,7 @@ local Games = {
   [994732206]  = "https://raw.githubusercontent.com/VoidDeveloper67/VoidHub-Scripts/refs/heads/main/VoidHub%20PvP", -- Blox Fruits
   [9787206684] = "https://raw.githubusercontent.com/VoidDeveloper67/VoidHub-Scripts/refs/heads/main/Be%20A%20Lucky%20Block", -- Be a Lucky Block
   [9098570654] = "https://raw.githubusercontent.com/VoidDeveloper67/VoidHub-Scripts/refs/heads/main/STA.luau", -- Survive The Apocalypse
-  [10004244222] = "https://api.luarmor.net/files/v4/loaders/3804f87ddf39261103b63af76f47e943.lua", -- Kick a Lucky Block
+  [10004244222] = "https://raw.githubusercontent.com/VoidDeveloper67/VoidHub-Scripts/refs/heads/main/Kick%20A%20Lucky%20Block", -- Kick a Lucky Block
   [9792947201] = "https://raw.githubusercontent.com/VoidDeveloper67/VoidHub-Scripts/refs/heads/main/Rng-Slime.luau", -- Slime RNG
   [9348272796] = "https://raw.githubusercontent.com/VoidDeveloper67/VoidHub-Scripts/refs/heads/main/Survive%20Zombie%20Arena.luau", -- Survive Zombie Arena
 }
